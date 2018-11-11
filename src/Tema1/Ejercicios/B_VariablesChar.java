d9d82b46206c1858ab44cc1b3fd7891964a730b5
sgGodino <sggodino@gmail.com>
2018-11-11 18:37:30 +0100

    EJERCICIO B

--- src/Tema1/Ejercicios/B_VariablesChar.java
+++ src/Tema1/Ejercicios/B_VariablesChar.java
@@ -11,6 +11,11 @@ import java.util.Scanner;
 public class B_VariablesChar {
 
     public static void main(String[] args) {
+        //COMO VAMOS A INTRRODUCIR UN CHAR, SE TIENE Q PONER ESTO
+        Scanner capt = new Scanner(System.in);
+        char a = capt.nextLine().charAt(0);
+        //PARA MOSTRAR LO Q INTRODUCES, SE TIENE Q PONER ESTO
+        System.out.print(a);
     }
 
 }
